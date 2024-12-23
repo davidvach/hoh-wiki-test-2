b2386b7962d0051a2002f3dd0d30296566df72d0
davidvach <vachalek.david@gmail.com>
2024-12-23 16:57:46 +0100

    test

---
+++ src/components/home/sectionsDefinition.js
@@ -0,0 +1,15 @@
+const sectionsDefinition = {
+    page: "",
+    tabs: [
+        {
+            id: 1,
+            url: "",
+            name: "Intro",
+            img: "",
+            show: false,
+            dynamicSegments: [],
+        },
+    ]
+}
+
+export default sectionsDefinition;
